c486b7915c77157d12adabee9fa0cf9f0d424ade
Mihaly Varga <mihaly.varga@etu.unice.fr>
2018-03-12 13:09:06 +0100

    Menu created with icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,60 @@
-import {Component} from '@angular/core';
-import {Platform} from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {Nav, Platform} from 'ionic-angular';
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 import {TabsPage} from "../pages/tabs/tabs";
+import {HomePage} from "../pages/home/home";
+import {WeatherPage} from "../pages/weather/weather";
+import {AddPlacePage} from "../pages/add-place/add-place";
+import {ContactPage} from "../pages/contact/contact";
+import {AboutPage} from "../pages/about/about";
 
 
 @Component({
 	templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
 	rootPage: any = TabsPage;
 
-	constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-		platform.ready().then(() => {
-			// Okay, so the platform is ready and our plugins are available.
-			// Here you can do any higher level native things you might need.
-			statusBar.styleDefault();
-			splashScreen.hide();
-		});
-	}
+	pages: Array<{title: string, component: any, icon: string}>;
+
+	// constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+	// 	platform.ready().then(() => {
+	// 		// Okay, so the platform is ready and our plugins are available.
+	// 		// Here you can do any higher level native things you might need.
+	// 		statusBar.styleDefault();
+	// 		splashScreen.hide();
+	// 	});
+	// }
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+    this.initializeApp();
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Home', component: HomePage, icon: "home"},
+      { title: 'Add Place', component: AddPlacePage, icon: "add-circle"},
+      { title: 'Weather', component: WeatherPage, icon: "cloud" },
+      { title: 'Contact', component: ContactPage, icon: "contacts" },
+      { title: 'About', component: AboutPage, icon: "information-circle" }
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
 }
 
